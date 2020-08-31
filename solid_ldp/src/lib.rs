d0433f0190221b00689801ce8448cba907019718
Florian Fromm <flrn.frmm@gmail.com>
2020-08-31 23:15:30 +0200

    Add base structures to represent LDPRs

--- solid_ldp/src/lib.rs
+++ solid_ldp/src/lib.rs
@@ -1,7 +1,36 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+pub enum Resource {
+    RDF(Container),
+    NonRDF(RawResource)
+}
+
+pub enum ContainerType {
+    Basic,
+    Direct,
+    Indirect
+}
+
+pub struct Container {
+    container_type: ContainerType
+}
+
+impl Container {
+    pub fn new(container_type: ContainerType) -> Self {
+        Self { container_type }
+    }
+}
+
+pub enum RawResourceType {
+    Text,
+    Image,
+    Blob
+}
+pub struct RawResource {
+    raw_resource_type: RawResourceType,
+    data: Vec<u8>
+}
+
+impl RawResource {
+    pub fn new(raw_resource_type: RawResourceType, data: Vec<u8>) -> Self {
+        Self { raw_resource_type, data }
     }
 }
